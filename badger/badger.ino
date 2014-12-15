a431350f76d0ca608187982c8adc79ccd71caeb5
Olivier <olivier@spaceconcordia.ca>
2014-12-15 16:08:49 -0500

    arduino communication fix, started flaps ordering

--- badger/badger.ino
+++ badger/badger.ino
@@ -2,121 +2,115 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
-#define BADGER_ADDRESS 0x9
-#define LOADER_ADDRESS 0x8
+#define BADGER_ADDRESS 0x20
+#define LOADER_ADDRESS 0x21
 
-#define NUM_SERVOS 8
+#define NUM_SERVOS 4
 #define OPEN 1
 #define CLOSE 0
-#define SERVOMIN  150 
-#define SERVOMAX  600 
+#define MAX_BAG_COUNT 8
 
 Adafruit_PWMServoDriver servos = Adafruit_PWMServoDriver();
 
 // Order to close/open flaps
-int index = 0;
-int close_order[NUM_SERVOS] = {0, 1, 2, 3, 4, 5, 6, 7};
-int open_order[NUM_SERVOS] = {0, 1, 2, 3, 4, 5, 6, 7};
+const int closeOrder[8] = {1, 5, 2, 6, 3, 7, 4, 8};
 
 int count = 0;
-const int buttonOne = 2;
+const int buttonOne = A0;
 const int buttonTwo = 3;
 
 boolean all_flaps_closed = false;
 
+int bag_count = 0;
+
 void setup() {
-  pinMode(13, OUTPUT);
-  pinMode(2, INPUT);
+  pinMode(buttonOne, INPUT);
+  pinMode(buttonTwo, INPUT);
+
   Serial.begin(9600);
+  
+  servos.begin();
+  servos.setPWMFreq(100); 
+  
+  // Open all but bottom flaps
+  for(int i = 2; i <= NUM_SERVOS; ++i) {
+    servos.setPWM(closeOrder[i], 0, 400);
+    delay(500);
+  }
+  
+  for(int i = 0; i < 2; ++i) {
+    servos.setPWM(closeOrder[i], 0, 1100);
+    delay(500);
+  }
+  
   Wire.begin(BADGER_ADDRESS);
   Wire.onReceive(receiveEvent);
-  Wire.onRequest(requestEvent);
-  servos.begin();
-  servos.setPWMFreq(100);
-
-  servos.setPWM(0, 0, map( 0, 0, 1000, 0, 4096 ));  
 }
 
 boolean flag = true;
+static int servoIndex = 0;
+
+volatile byte received = false;
 
 void loop() 
-{        
-  static int count = 0;
+{     
   static boolean ready = true;
+  
+  if (received) {
+//    Serial.println("got something");
+    closeFlaps();
+    received = false;
+  }
+  else {
+//    Serial.println("Nothing yet");
+  }
+//  delay(500);
+
   if( !ready || digitalRead(buttonOne) ){
     ready = true;
   } else{
     ready = false;
-    count = (count + 1) % 2;
-    switch(count){
-      case 0:
-        servos.setPWM(0, 0, 1000);
-        Serial.println("Close");
-        break;
-      case 1:
-        servos.setPWM(0, 0, 400);
-        Serial.println("Open");
-        break;
-    }    
+    openFlaps();
     delay(500);
-  }
+  }  
 }
 
 // function that executes when data is received from loader
 void receiveEvent(int bytes)
 {
-  int servo_to_set, servo_state;
-
+  int towerToClose;
+  received = true;
   while(Wire.available())
   {
-    servo_to_set = Wire.read();   
-    servo_state = Wire.read();   
-  }
-  Serial.print("Servo: ");
-  Serial.print(servo_to_set);
-  Serial.print(" set to ");
-  Serial.print(servo_state);   
-  Serial.println();
-  
-// toggleFlaps(servo_to_set, servo_state);
- closeFlaps();
-  confirmMessage();
-}
-
-// toggle a a servo to open or close
-void toggleFlaps(int servonum, int state)
-{
-  uint16_t pulselen = 1100;  
-  if(state == OPEN) 
-     pulselen = 400;
-
-  servos.setPWM(servonum, 0, pulselen);
+    towerToClose = Wire.read();
+  }  
 }
 
 // Cycles through all flaps to close in order, closes 1 flap
 void closeFlaps()
 {
-  servos.setPWM(close_order[index], 0, 1100);
-  index++;
-  
-  if(index == 8)
-  {
-    all_flaps_closed = true;
-    index = 0;
+  if(bag_count >= 8) {
+     Serial.println("wtf got more than 8 bags"); 
+     return;
   }
+  
+  Serial.print("Closing servo ");
+  Serial.println(closeOrder[bag_count]);
+  servos.setPWM(closeOrder[bag_count++], 0, 1100);
+  delay(500);
 }
 
 // Opens a flap
 void openFlaps()
 {
-  servos.setPWM(close_order[index], 0, 400);
-  index++;
-  
-  if(index == 8)
-  {
-    all_flaps_closed = true;   
-    index = 0;
+  if(bag_count < 0) {
+    Serial.println("wtf got negative bags");
   }
+  
+  Serial.print("Opening servo ");
+  Serial.println(closeOrder[MAX_BAG_COUNT - bag_count]);
+  servos.setPWM(closeOrder[MAX_BAG_COUNT - bag_count--], 0, 400);
+  delay(500);
 }
 
 void closeAllFlaps()
@@ -131,12 +125,13 @@ void closeAllFlaps()
 void confirmMessage()
 {
   Wire.beginTransmission(LOADER_ADDRESS); 
-  Wire.write(254);  
+  Wire.write(254);
   Wire.endTransmission();
 }
-
+/*
 // function that executes when data is requested by loader
 void requestEvent()
 {
   Wire.write("Responding to loader request"); // 28 bytes
 }
+*/
